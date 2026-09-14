89323fddd5860afc1f375a9de1e959ed09e99ec3
İzzet Şahin <izzetsahin86@icloud.com>
2026-09-14 18:30:14 +0300

    Enable JSON API requests

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const publicDir = path.join(__dirname, '..', 'public');
 
+app.use(express.json({ limit: '1mb' }));
 app.use(express.static(publicDir));
 app.use('/api', apiRouter);
 

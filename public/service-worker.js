bf59c3977f749be2f40084bb5ead59e201268cf6
İzzet Şahin <izzetsahin86@icloud.com>
2026-09-16 21:23:29 +0300

    Refresh cache for complete Accounts layout fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'siberdeyz-pwa-20260916-account-transfer-layout-fix-1';
+const CACHE_NAME = 'siberdeyz-pwa-20260916-accounts-layout-complete-fix-1';
 
 const APP_SHELL = [
   '/',
